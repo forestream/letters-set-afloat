7caf0f8167d6d10d8f45c98bfc1ae406a3d3c3e7
hanbin <rj2303@naver.com>
2024-07-07 00:52:29 +0900

    메인 페이지 조회 이벤트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
+import Script from "next/script";
 import Background from "./background";
 import Foreground from "./foreground";
 import styles from "./page.module.css";
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<Background />
-			<Foreground />
-		</main>
+		<>
+			{/* <!-- Event snippet for 페이지 조회 conversion page --> */}
+			<Script id="ga-event-conversion">
+				{`
+				gtag('event', 'conversion', {
+				'send_to': 'AW-16636190539/6B1PCO3mysAZEMu-4Pw9',
+				'value': 1.0,
+				'currency': 'KRW'
+				});
+				`}
+			</Script>
+
+			<main className={styles.main}>
+				<Background />
+				<Foreground />
+			</main>
+		</>
 	);
 }
