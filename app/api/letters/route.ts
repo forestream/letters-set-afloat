b4913686a3b39937251546ebaf6e9cbfabd78f38
hanbin <rj2303@naver.com>
2024-07-09 10:59:18 +0900

    편지 전송 시 줄바꿈 포함한 JSON 데이터로 전송하도록 변경

--- app/api/letters/route.ts
+++ app/api/letters/route.ts
@@ -3,11 +3,12 @@ import { Timestamp, addDoc, collection } from "firebase/firestore";
 
 export async function POST(request: Request) {
 	const body = await request.json();
+	const letter = JSON.stringify(body.letter);
 
 	try {
 		await addDoc(collection(db, "letters"), {
 			sentAt: Timestamp.now(),
-			letter: body.letter,
+			letter,
 		});
 
 		return new Response(null, {
@@ -15,6 +16,7 @@ export async function POST(request: Request) {
 			statusText: "Created",
 		});
 	} catch (error: any) {
+		console.log(error);
 		return new Response("Error adding document: " + error, {
 			status: 400,
 			statusText: "Bad Request",
